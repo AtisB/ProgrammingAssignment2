1c84a56301365e56338a68d906a587d8c6fe9fb0
alexdum <alexdum@MacBook-Pro.local>
2014-05-23 10:45:30 +0300

    pair of functions that cache the inverse of a matr
    pair of functions that cache the inverse of a matrix

---
+++ cachematrix.R
@@ -0,0 +1,31 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  invrs <- NULL
+  set <- function(y){
+    x <<- y
+    invrs <<- NULL
+  }
+  get <- function() x
+  setinvrs <- function(solve) invrs <<- solve
+  getinvrs <- function() invrs
+  list(set = set, get = get, 
+       setinvrs = setinvrs, getinvrs = getinvrs)
+}
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+  invrs <- x$getinvrs()
+  if(!is.null(invrs)){
+    message("getting cached data")
+    return(invrs)
+  }
+  data <- x$get()
+  invrs <- solve(data, ...)
+  x$setinvrs(invrs)
+  invrs
+}
